a3ad2fad64b92fba0e11974d64b18a039c08ffc1
hamachan <yusuke9127@gmail.com>
2025-10-20 06:59:49 +0900

    add ti command not found error message

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"os/exec"
 	"ruby-ti-lsp/lsp"
 )
 
 func main() {
+	if _, err := exec.LookPath("ti"); err != nil {
+		fmt.Println("Error: 'ti' command not found in PATH")
+		fmt.Println("Ruby-TI LSP requires the 'ti' command to be installed")
+		fmt.Println("Please install Ruby-TI: https://github.com/engneer-hamachan/ruby-ti")
+		os.Exit(1)
+	}
+
 	server := lsp.NewServer()
 	server.RunStdio()
 }
